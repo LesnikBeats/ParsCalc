8482c2ad4f2a24ce6a8bbe3fa265205edcf28c7e
Ivan Kalesnik <ivan.kalesnik1@gmail.com>
2022-12-18 14:46:36 +0300

    C# parser

---
+++ CSharp model/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using ConsoleApp1;
+class Program
+{
+    static void Main(string[] args)
+    {
+        while (true) //Бесконечный цикл
+        {
+            Console.Write("Введите выражение: "); //Предлагаем ввести выражение
+            Console.WriteLine(RPN.Calculate(Console.ReadLine())); //Считываем, и выводим результат
+        }
+    }
+}
